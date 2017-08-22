dda7ebaca0647ae5585f8f87fda3a02ede9d6ed8
Diwakar Chand <dchand@covermymeds.com>
2017-08-22 14:41:32 -0400

    adding files to a contaner

--- create_container_blobs.rb
+++ create_container_blobs.rb
@@ -1,7 +1,13 @@
 require './azure_storage_helper.rb'
 
-begin  
+begin
   container = @azure_blob_service.create_container("samplecontainer")
+  Dir.foreach(Dir.pwd+'/data') do |blob_item|
+    next if blob_item == '.' or blob_item == '..'
+    content = ::File.open(Dir.pwd+'/data/' + blob_item, 'rb') { |file| file.read }
+    puts "Adding... #{blob_item} to #{container.name}"
+    @azure_blob_service.create_block_blob(container.name, blob_item, content)
+  end
 rescue Exception => e
   #binding.pry
   puts e.message
